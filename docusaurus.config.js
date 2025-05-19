063f2d3cf7295202ab32b91d3077b512043aa721
Konrad <k.janecki184@gmail.com>
2025-05-19 17:12:28 +0200

    menu fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,9 +86,9 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Knowledge base',
           },
-          {to: '/docs', label: 'Knowledge base', position: 'left'},
+          {to: '/blog', label: 'Blog', position: 'left'},
         ],
       },
       footer: {
